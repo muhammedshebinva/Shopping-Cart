7c18ccb0dc2777a997498ac20a0ffd4bbffd3004
muhammedshebinva <muhammedshebinva@gmail.com>
2021-07-29 12:44:48 +0530

    find the uplode image id from data ,move that image into the created folder (product-images)in public with the id to the image

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ var logger = require('morgan');
 
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
-var hbs=require('express-handlebars')
+var hbs=require('express-handlebars');
 
 var app = express();
-var fileUploade=require('express-fileupload')
+var fileUploade=require('express-fileupload');
+var db=require('./config/connection')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,8 +24,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUploade())
 
+db.connect((err)=>{
+  if(err) 
+  console.log("connection error"+err)
+  else 
+  console.log("Database connected to part 27017")
+})
+
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
+const {render}=require('../app');
 var router = express.Router();
-
+var productHelpers=require('../helpers/product-helpers')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
@@ -38,8 +39,22 @@ router.get('/add-product',(req,res)=>{
 })
 
 router.post('/add-product',(req,res)=>{
-  console.log(req.body);
-  console.log(req.files.Image);
+ 
+
+  productHelpers.addProduct(req.body,(id)=>{
+    let image=req.files.Image
+    console.log(id);
+    image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
+      if(!err){
+        res.render("admin/add-product")
+      }else{
+        console.log(err);
+      }
+    })
+    
+   
+  })
+
 })
 
 module.exports = router;
