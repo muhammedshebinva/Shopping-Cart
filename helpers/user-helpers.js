2d0aa71e307a1d7d07f3ff4c293029bb4168f07c
muhammedshebinva <muhammedshebinva@gmail.com>
2021-08-13 15:08:51 +0530

    session , login-logout

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -17,18 +17,23 @@ module.exports={
     doLogin:(userData)=>{
         return new Promise(async(resolve,reject)=>{
             let loginStatus=false
-            let responce={}
+            let response={}
             let user=await db.get().collection(collection.USER_COLLECTION).findOne({Email:userData.Email})
             if(user){
                 bcrypt.compare(userData.Password,user.Password).then((status)=>{
                     if(status){
                         console.log("login success");
+                        response.user=user
+                        response.status=true
+                        resolve(response)
                     }else{
                         console.log("login Failed");
+                        resolve({status:false})
                     }
                 })
             }else{
                 console.log("login Failed");
+                resolve({status:false})
             }
         })
     }
