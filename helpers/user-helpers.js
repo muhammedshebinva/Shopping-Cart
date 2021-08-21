c967cc670b8a7dba3f09b6db0d201829fabba686
muhammedshebinva <muhammedshebinva@gmail.com>
2021-08-21 11:05:38 +0530

    product count quantity incresed and decresed and removed product

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -56,7 +56,7 @@ module.exports={
                 console.log(proExist);
                 if(proExist!=-1){
                     db.get().collection(collection.CART_COLLECTION)
-                    .updateOne({'products.item':objectId(proId)},
+                    .updateOne({user:objectId(userId),'products.item':objectId(proId)},
                     {
                         $inc:{'products.$.quantity':1}
                     }
@@ -108,6 +108,11 @@ module.exports={
                         foreignField:'_id',
                         as:'product'
                     }
+                },
+                {
+                    $project:{
+                        item:1,quantity:1,product:{$arrayElemAt:['$product',0]}
+                    }
                 }
                 
             ]).toArray()
@@ -122,6 +127,32 @@ module.exports={
                 count=cart.products.length
             }
             resolve(count)
+        })
+    },
+    changeProductQuantity:(details)=>{
+        details.count=parseInt(details.count)
+        details.quantity=parseInt(details.quantity)
+        
+        return new Promise((resolve,reject)=>{
+            if(details.count==-1 && details.quantity==1){
+                db.get().collection(collection.CART_COLLECTION)
+                .updateOne({_id:objectId(details.cart)},
+                {
+                    $pull:{products:{item:objectId(details.product)}}
+                }
+                ).then((responce)=>{
+                    resolve({removeProduct:true})
+                })
+            }else{
+            db.get().collection(collection.CART_COLLECTION)
+                    .updateOne({_id:objectId(details.cart),'products.item':objectId(details.product)},
+                    {
+                        $inc:{'products.$.quantity':details.count}
+                    }
+                    ).then((responce)=>{
+                        resolve(true)
+                    })
+            }
         })
     }
 }
